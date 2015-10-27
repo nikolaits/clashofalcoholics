a5be74c730759c7877389e49ad6493032031a778
Veselin Smokov <wesmok87@gmail.com>
2015-10-27 07:26:57 +0000

    Add models assosiations (UPR.  3)

--- app/models/building_level.rb
+++ app/models/building_level.rb
@@ -1,3 +1,3 @@
 class BuildingLevel  < ActiveRecord::Base
-
+	belongs_to :building
 end
--- app/models/district_building.rb
+++ app/models/district_building.rb
@@ -1,3 +1,4 @@
 class DistrictBuilding < ActiveRecord::Base
-	
+	belongs_to :building
+	belongs_to :district
 end
